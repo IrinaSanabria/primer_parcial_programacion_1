fbde75c317a25779d511109dccc75f2f04567f54
Irina Sanabria <irinasanabria@gmail.com>
2025-05-30 14:04:00 -0300

    Entre 1° Parcial ejercicios 3 y 4

--- funciones.py
+++ funciones.py
@@ -29,3 +29,26 @@ def porcentaje_aprobados(matriz): # Función para calcular el porcentaje de apro
                 aprobados += 1
         porcentaje = (aprobados * 100) / total_examenes # Se calcula el porcentaje
         print(f"Alumno {i+1}: {porcentaje}% de exámenes aprobados.")
+
+def mejor_promedio(matriz): # Función que calcula el mejor promedio entre los alumnos
+    indice_mayor = -1
+    mayor_promedio = 0.0
+    for i in range(len(matriz)):
+        total = 0
+        suma = 0
+        for j in range(len(matriz[i])):
+            suma += matriz[i][j]  # Acumulamos las notas
+            total += 1
+        promedio = suma / total  # El promedio de alumno
+        if indice_mayor == -1 or promedio > mayor_promedio:
+            mayor_promedio = promedio  # Se guarda el mejor promedio
+            indice_mayor = i  # También guardo el indice del alumno
+    return indice_mayor, mayor_promedio # Retorna el indice del alumno y su promedio (el mejor promedio)
+
+def buscar_nota(matriz, buscada): # Función para buscar la posición de alguna nota
+    posiciones = []  # Lista que guarda las posiciones
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == buscada:
+                posiciones.append((i, j))
+    return posiciones # Devuelve la posicion de la nota
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from funciones import cargar_matriz_notas, porcentaje_aprobados
+from funciones import cargar_matriz_notas, porcentaje_aprobados, mejor_promedio, buscar_nota
 
 def menu():
     matriz = []  # Inicializamos la matriz vacía
@@ -6,7 +6,9 @@ def menu():
         print("\n----- MENÚ -----")
         print("1. Cargar matriz de notas")
         print("2. Mostrar porcentaje de aprobados por alumno")
-        print("3. Salir")
+        print("3. Mostrar mejor promedio")
+        print("4. Buscar nota exacta")
+        print("5. Salir")
         opcion = input("Seleccione una opción: ")
         match opcion:
             case "1":
@@ -19,6 +21,23 @@ def menu():
                 else:
                     print("Debe cargar la matriz primero.")
             case "3":
+                if matriz:
+                    indice_mayor, mayor_promedio = mejor_promedio(matriz) # Se llama a la función que devuelve el mejor promedio
+                    print(f"El alumno con mejor promedio es {indice_mayor + 1} con un promedio de {mayor_promedio}.")
+                else:
+                    print("Primero debe cargar las notas.")
+            case "4":
+                if matriz:
+                    nota = int(input("Ingrese la nota a buscar: "))
+                    posiciones = buscar_nota(matriz, nota) # Llamado a la función que busca la nota
+                    if posiciones:
+                        for fila, col in posiciones:
+                            print(f"Se encontro la nota en la fila {fila+1}, columna {col+1}")
+                    else:
+                        print("La nota no se encontro en la matriz.")
+                else:
+                    print("Primero debe cargar las notas.")
+            case "5":
                 print("Saliendo del programa...")
                 break
             case _:
